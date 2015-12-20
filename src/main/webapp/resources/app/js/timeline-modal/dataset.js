2ba6c2c7dc24c6da1a71305145da4b64b1234400
Manish Agrawal <magrawal6055@gmail.com>
2015-12-20 21:15:54 +0800

    edited generated coupon

---
+++ src/main/webapp/resources/app/js/timeline-modal/dataset.js
@@ -0,0 +1,9 @@
+
+
+var couponDetailData = [["Day Breaker Half Marathon Gurgaon HR","3500", "5", "20"],
+                        ["Golden Jubilee Half New Delhi DL", "3800", "6","25"],
+                        ["Run For the health of Delhi", "3000", "6", "30"]];
+
+var registrationDetailData = [["Day Breaker Half Marathon Gurgaon HR","3500", "2", "30"],
+                        ["Golden Jubilee Half New Delhi DL", "3800", "3","40"],
+                        ["Run For the health of Delhi", "3000", "4", "50"]];
